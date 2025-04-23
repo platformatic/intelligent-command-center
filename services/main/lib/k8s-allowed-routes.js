b914fd3c83e487e13bb039fc9c76a61447a7cfbf
marcopiraccini <marco.piraccini@gmail.com>
2025-04-23 10:40:55 +0200

    K8S JWT auth

---
+++ services/main/lib/k8s-allowed-routes.js
@@ -0,0 +1,33 @@
+'use strict'
+
+// List of routes that can be authorized via K8s JWT authentication
+const k8sAllowedRoutes = [
+  // wattpro (zio) called routes
+  [/\/control-plane\/pods\/[a-zA-z0-9-]+\/instance/, ['POST']],
+  [/\/control-plane\/pods\/[a-zA-z0-9-]+\/instance\/state/, ['POST']],
+  [/\/compliance\/metadata/, ['POST']]
+]
+
+function methodIsAllowed (method, allowedMethods) {
+  return allowedMethods === '*' || allowedMethods.includes(method)
+}
+
+async function isUrlAllowed (currentUrl, method) {
+  if (!currentUrl) {
+    return false
+  }
+  const splitForQueryParams = currentUrl.split('?')
+  const urlToCheck = splitForQueryParams[0]
+  console.log('urlToCheck', urlToCheck)
+  return k8sAllowedRoutes.filter((routeData) => {
+    const path = routeData[0]
+    const allowedMethods = routeData[1] ? routeData[1] : '*'
+    if (path instanceof RegExp) {
+      return urlToCheck.match(path) && methodIsAllowed(method, allowedMethods)
+    } else {
+      return urlToCheck.startsWith(path) && methodIsAllowed(method, allowedMethods)
+    }
+  }).length > 0
+}
+
+module.exports = isUrlAllowed
