93e0bc63b722f02d2dcf27250293c17625bd954f
Ivan Tymoshenko <ivan@tymoshenko.me>
2025-05-12 13:26:56 +0200

    feat: save min and max application pods (#61)

---
+++ services/scaler/plugins/scale-configs.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const fp = require('fastify-plugin')
+
+module.exports = fp(async function (app) {
+  app.decorate('getScaleConfig', async (applicationId) => {
+    const scaleConfigs = await app.platformatic.entities.applicationScaleConfig.find({
+      where: { applicationId: { eq: applicationId } },
+      orderBy: { createdAt: 'desc' },
+      limit: 1
+    })
+    return scaleConfigs.length > 0 ? scaleConfigs[0] : null
+  })
+
+  app.decorate('saveScaleConfig', async (applicationId, config) => {
+    const scaleConfig = await app.platformatic.entities.applicationScaleConfig.save({
+      input: {
+        applicationId,
+        minPods: config.minPods,
+        maxPods: config.maxPods
+      }
+    })
+    return scaleConfig
+  })
+}, { name: 'scale-config' })
