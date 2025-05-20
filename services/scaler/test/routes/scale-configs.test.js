8eb45d66772c83901163f89ac20c533ad9323cf9
Ivan Tymoshenko <ivan@tymoshenko.me>
2025-05-20 20:27:22 +0200

    feat: scale to min and max values (#91)

--- services/scaler/test/routes/scale-configs.test.js
+++ services/scaler/test/routes/scale-configs.test.js
@@ -2,16 +2,52 @@
 
 const assert = require('node:assert/strict')
 const { test } = require('node:test')
-const { buildServer } = require('../helper')
 const { randomUUID } = require('node:crypto')
+const { buildServer, startMachinist } = require('../helper')
 
-test('should save application min and max pods', async (t) => {
-  const server = await buildServer(t)
+test('should save application min and max pods (triggers scale up)', async (t) => {
   const applicationId = randomUUID()
+  const deploymentId = randomUUID()
+  const controllerId = 'controller-id'
+  const namespace = 'platformatic'
+  const podId = 'pod-id'
+  const kind = 'Controller'
+  const apiVersion = 'v1'
 
+  const server = await buildServer(t)
   t.after(async () => {
     await server.close()
   })
+
+  const controllerUpdates = []
+  await startMachinist(t, {
+    getPodController: () => ({
+      name: controllerId,
+      kind,
+      apiVersion,
+      replicas: 1
+    }),
+    setPodController: (controller) => {
+      controllerUpdates.push(controller)
+    }
+  })
+
+  {
+    const { statusCode } = await server.inject({
+      method: 'POST',
+      url: '/controllers',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        applicationId,
+        deploymentId,
+        namespace,
+        podId
+      })
+    })
+    assert.strictEqual(statusCode, 200)
+  }
 
   {
     const { statusCode, body } = await server.inject({
@@ -41,15 +77,140 @@ test('should save application min and max pods', async (t) => {
     assert.strictEqual(scaleConfig.minPods, 2)
     assert.strictEqual(scaleConfig.maxPods, 4)
   }
+
+  assert.strictEqual(controllerUpdates.length, 1)
+
+  const controller = controllerUpdates[0]
+  assert.strictEqual(controller.controllerId, controllerId)
+  assert.strictEqual(controller.namespace, namespace)
+  assert.strictEqual(controller.replicas, 2)
 })
 
-test('should keep history of configs and get only the last one', async (t) => {
-  const server = await buildServer(t)
+test('should save application min and max pods (triggers scale down)', async (t) => {
   const applicationId = randomUUID()
+  const deploymentId = randomUUID()
+  const controllerId = 'controller-id'
+  const namespace = 'platformatic'
+  const podId = 'pod-id'
+  const kind = 'Controller'
+  const apiVersion = 'v1'
 
+  const server = await buildServer(t)
   t.after(async () => {
     await server.close()
   })
+
+  const controllerUpdates = []
+  await startMachinist(t, {
+    getPodController: () => ({
+      name: controllerId,
+      kind,
+      apiVersion,
+      replicas: 10
+    }),
+    setPodController: (controller) => {
+      controllerUpdates.push(controller)
+    }
+  })
+
+  {
+    const { statusCode } = await server.inject({
+      method: 'POST',
+      url: '/controllers',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        applicationId,
+        deploymentId,
+        namespace,
+        podId
+      })
+    })
+    assert.strictEqual(statusCode, 200)
+  }
+
+  {
+    const { statusCode, body } = await server.inject({
+      method: 'POST',
+      url: `/applications/${applicationId}/scale-configs`,
+      headers: {
+        'content-type': 'application/json'
+      },
+      payload: JSON.stringify({ minPods: 2, maxPods: 4 })
+    })
+
+    assert.strictEqual(statusCode, 200)
+
+    const data = JSON.parse(body)
+    assert.deepStrictEqual(data, { success: true })
+  }
+
+  {
+    const { statusCode, body } = await server.inject({
+      url: `/applications/${applicationId}/scale-configs`
+    })
+
+    assert.strictEqual(statusCode, 200)
+
+    const scaleConfig = JSON.parse(body)
+    assert.strictEqual(scaleConfig.applicationId, applicationId)
+    assert.strictEqual(scaleConfig.minPods, 2)
+    assert.strictEqual(scaleConfig.maxPods, 4)
+  }
+
+  assert.strictEqual(controllerUpdates.length, 1)
+
+  const controller = controllerUpdates[0]
+  assert.strictEqual(controller.controllerId, controllerId)
+  assert.strictEqual(controller.namespace, namespace)
+  assert.strictEqual(controller.replicas, 4)
+})
+
+test('should keep history of configs and get only the last one', async (t) => {
+  const applicationId = randomUUID()
+  const deploymentId = randomUUID()
+  const controllerId = 'controller-id'
+  const namespace = 'platformatic'
+  const podId = 'pod-id'
+  const kind = 'Controller'
+  const apiVersion = 'v1'
+
+  const server = await buildServer(t)
+  t.after(async () => {
+    await server.close()
+  })
+
+  const controllerUpdates = []
+  await startMachinist(t, {
+    getPodController: () => ({
+      name: controllerId,
+      kind,
+      apiVersion,
+      replicas: 1
+    }),
+    setPodController: (controller) => {
+      controllerUpdates.push(controller)
+    }
+  })
+
+  {
+    const { statusCode } = await server.inject({
+      method: 'POST',
+      url: '/controllers',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        applicationId,
+        deploymentId,
+        namespace,
+        podId
+      })
+    })
+    assert.strictEqual(statusCode, 200)
+  }
+
   for (let i = 1; i <= 10; i++) {
     const { statusCode } = await server.inject({
       method: 'POST',
@@ -72,4 +233,6 @@ test('should keep history of configs and get only the last one', async (t) => {
   assert.strictEqual(scaleConfig.applicationId, applicationId)
   assert.strictEqual(scaleConfig.minPods, 10)
   assert.strictEqual(scaleConfig.maxPods, 100)
+
+  assert.strictEqual(controllerUpdates.length, 9)
 })
