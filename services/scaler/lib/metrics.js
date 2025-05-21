7caa05f01a5897dc93e9b4a781d202a2b8aa8d16
Marco <marco.piraccini@gmail.com>
2025-05-21 14:44:18 +0200

    Scaler algorithm (#92)
    Scaler algorithm
    
    ---------
    
    Signed-off-by: marcopiraccini <marco.piraccini@gmail.com>

--- services/scaler/lib/metrics.js
+++ services/scaler/lib/metrics.js
@@ -102,6 +102,106 @@ class Metrics {
       throw err
     }
   }
+
+  async #queryAllApplicationsMetric (metricName, timeRange, step = 1) {
+    const now = Math.floor(Date.now() / 1000)
+    const start = now - (timeRange ?? this.timeRange)
+    const end = now
+
+    try {
+      const url = new URL('/api/v1/query_range', this.prometheusUrl)
+      const query = `${metricName}`
+
+      url.searchParams.append('query', query)
+      url.searchParams.append('start', start.toString())
+      url.searchParams.append('end', end.toString())
+      url.searchParams.append('step', step.toString())
+
+      this.log.debug({ url: url.toString() }, 'Querying Prometheus for all applications')
+
+      const { statusCode, body } = await request(url)
+
+      if (statusCode !== 200) {
+        const error = await body.text()
+        throw new Error(`Prometheus API error: ${error}`)
+      }
+
+      const data = await body.json()
+
+      if (data.status === 'success' && data.data?.result) {
+        // Group results by applicationId then by podId
+        const resultsByApplicationId = {}
+
+        for (const item of data.data.result) {
+          const applicationId = item?.metric?.applicationId || 'unknown'
+          const podId = item?.metric?.podId || item?.metric?.instanceId || 'unknown'
+
+          if (!resultsByApplicationId[applicationId]) {
+            resultsByApplicationId[applicationId] = {}
+          }
+
+          if (!resultsByApplicationId[applicationId][podId]) {
+            resultsByApplicationId[applicationId][podId] = []
+          }
+
+          resultsByApplicationId[applicationId][podId].push(item)
+        }
+
+        return resultsByApplicationId
+      } else {
+        this.log.warn({ data }, `Invalid response for metric ${metricName}`)
+        throw new Error(`Invalid response for metric ${metricName}`)
+      }
+    } catch (err) {
+      this.log.error({ err, metricName }, 'Error querying Prometheus for all applications')
+      throw err
+    }
+  }
+
+  async getAllApplicationsMetrics (timeRange, step = 1) {
+    try {
+      const effectiveTimeRange = timeRange ?? this.timeRange
+      const [heapSizeByApp, eventLoopUtilizationByApp] = await Promise.all([
+        this.#queryAllApplicationsMetric('nodejs_heap_size_total_bytes', effectiveTimeRange, step),
+        this.#queryAllApplicationsMetric('nodejs_eventloop_utilization', effectiveTimeRange, step)
+      ])
+
+      const appMetrics = {}
+
+      // Combine all application IDs from both metrics
+      const allAppIds = new Set([
+        ...Object.keys(heapSizeByApp),
+        ...Object.keys(eventLoopUtilizationByApp)
+      ])
+
+      for (const appId of allAppIds) {
+        const heapSizeForApp = heapSizeByApp[appId] || {}
+        const eventLoopUtilForApp = eventLoopUtilizationByApp[appId] || {}
+
+        // Combine all pod IDs for this application
+        const allPodIds = new Set([
+          ...Object.keys(heapSizeForApp),
+          ...Object.keys(eventLoopUtilForApp)
+        ])
+
+        const podMetrics = {}
+
+        for (const podId of allPodIds) {
+          podMetrics[podId] = {
+            heapSize: heapSizeForApp[podId] || [],
+            eventLoopUtilization: eventLoopUtilForApp[podId] || []
+          }
+        }
+
+        appMetrics[appId] = podMetrics
+      }
+
+      return appMetrics
+    } catch (err) {
+      this.log.error({ err }, 'Error getting metrics for all applications')
+      throw err
+    }
+  }
 }
 
 module.exports = Metrics
