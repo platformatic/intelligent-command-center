3d4927f73ea4c52919f341c73e91b0c4caf4f854
Marco <marco.piraccini@gmail.com>
2025-05-21 15:33:07 +0200

    Periodic trigger of scaler using metrics for scaling down (#89)
    * added preiodic trigger of scaling for scale down
    
    * fixup lint
    
    Signed-off-by: marcopiraccini <marco.piraccini@gmail.com>
    
    * fixed tests after rebase
    
    Signed-off-by: marcopiraccini <marco.piraccini@gmail.com>
    
    ---------
    
    Signed-off-by: marcopiraccini <marco.piraccini@gmail.com>

--- services/scaler/plugins/scaler.js
+++ services/scaler/plugins/scaler.js
@@ -1,12 +1,16 @@
 'use strict'
 
 const fp = require('fastify-plugin')
+const { setTimeout } = require('timers/promises')
 const createLeaderElector = require('../lib/leader')
 
 async function plugin (app) {
   const lock = Number(process.env.PLT_SCALER_LOCK) || 4242
   const poll = Number(process.env.PLT_SCALER_LEADER_POLL) || 10000
+  const periodicTriggerInterval = Number(process.env.PLT_SCALER_PERIODIC_TRIGGER) || 60000 // The default is only for tests
   const { db } = app.platformatic
+  let periodicTriggerController = null
+  let isLeader = false
 
   // Only one instance of the scaler receive and process the notification
   const scaler = createLeaderElector({
@@ -15,10 +19,71 @@ async function plugin (app) {
     poll,
     channel: 'trigger_scaler',
     log: app.log,
-    onNotification: app.scalerExecutor.checkScalingOnAlert
+    onNotification: app.scalerExecutor.checkScalingOnAlert,
+    onLeadershipChange: (newIsLeader) => {
+      if (newIsLeader !== isLeader) {
+        isLeader = newIsLeader
+        app.log.info({ isLeader }, 'Leadership status changed')
+
+        if (isLeader) {
+          startPeriodicTrigger()
+        } else {
+          stopPeriodicTrigger()
+        }
+      }
+    }
   })
 
+  async function startPeriodicTrigger () {
+    if (periodicTriggerController && !periodicTriggerController.signal.aborted) {
+      return // Already running
+    }
+
+    periodicTriggerController = new AbortController()
+    const { signal } = periodicTriggerController
+
+    app.log.info({ interval: periodicTriggerInterval }, 'Starting periodic scaler trigger')
+
+    // Continuous loop that executes the periodic trigger (only runs on leader)
+    try {
+      while (!signal.aborted) {
+        await setTimeout(periodicTriggerInterval, null, { signal })
+
+        if (isLeader) {
+          app.log.info('Executing periodic scaler trigger')
+          try {
+            // Execute without a specific podId - this will trigger a global scaling evaluation
+            await app.scalerExecutor.checkScalingOnMetrics()
+          } catch (err) {
+            app.log.error({ err }, 'Error during periodic trigger execution')
+          }
+        }
+      }
+    } catch (err) {
+      if (err.name !== 'AbortError') {
+        app.log.error({ err }, 'Error in periodic trigger loop')
+        // Restart the loop after a short delay if not aborted
+        if (!signal.aborted) {
+          setTimeout(1000).then(() => {
+            if (isLeader) {
+              startPeriodicTrigger()
+            }
+          })
+        }
+      }
+    }
+  }
+
+  function stopPeriodicTrigger () {
+    if (periodicTriggerController) {
+      periodicTriggerController.abort()
+      periodicTriggerController = null
+      app.log.info('Stopped periodic scaler trigger')
+    }
+  }
+
   app.addHook('onClose', async () => {
+    stopPeriodicTrigger()
     await scaler.stop()
   })
 
@@ -27,7 +92,7 @@ async function plugin (app) {
   })
 
   app.decorate('isScalerLeader', function () {
-    return scaler.isLeader()
+    return isLeader
   })
 
   scaler.start()
