0a77dbaf9b39c53f334aeb2c460872991e67a337
Leonardo Rossi <leonardo.rossi@gmail.com>
2025-05-27 09:54:14 +0200

    Update UI for Pod Details (#120)

--- services/frontend/src/paths.js
+++ services/frontend/src/paths.js
@@ -3,3 +3,6 @@ export const APPLICATION_DETAILS_ALL_SERVICES_DETAIL = '/applications/:applicati
 export const APPLICATION_DETAILS_ALL_ACTIVITIES = '/applications/:applicationId/activities'
 export const APPLICATION_DETAILS_SCHEDULED_JOBS = '/applications/:applicationId/scheduled-jobs'
 export const APPLICATION_DETAILS_SCHEDULED_JOB_DETAIL = '/applications/:applicationId/scheduled-jobs/:id'
+
+export const AUTOSCALER_POD_DETAIL_PATH = '/applications/:applicationId/autoscaler/:podId'
+export const AUTOSCALER_POD_DETAIL_SERVICES_PATH = '/applications/:applicationId/autoscaler/:podId/services'
