257125f029889032d7637e631ee55dc3f1f263e4
Marco <marco.piraccini@gmail.com>
2025-09-15 17:42:05 +0200

    Version checks compatible latest (#381)
    * Compare the runtime version with the compatible major
    
    Signed-off-by: marcopiraccini <marco.piraccini@gmail.com>
    
    * Compare the runtime version with the compatible major
    
    Signed-off-by: marcopiraccini <marco.piraccini@gmail.com>
    
    ---------
    
    Signed-off-by: marcopiraccini <marco.piraccini@gmail.com>

--- services/main/lib/plugins/packages.js
+++ services/main/lib/plugins/packages.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const { request } = require('undici')
+const semver = require('semver')
 const fp = require('fastify-plugin')
 
 /** @param {import('fastify').FastifyInstance} app */
@@ -17,7 +18,41 @@ module.exports = fp(async function (app) {
       const res = results[i]
       if (res.statusCode === 200) {
         const json = await res.body.json()
-        output[packageName] = json['dist-tags'].latest
+
+        // Get all versions, but filter out pre-releases, betas, etc.
+        const versions = Object.keys(json.versions).filter(v => {
+          return semver.valid(v) && !semver.prerelease(v)
+        })
+
+        const latestVersion = json['dist-tags'].latest
+        const latestMajor = semver.major(latestVersion)
+
+        // Group versions by major version
+        const versionsByMajor = {}
+        versions.forEach(v => {
+          const major = semver.major(v)
+          if (!versionsByMajor[major]) {
+            versionsByMajor[major] = []
+          }
+          versionsByMajor[major].push(v)
+        })
+
+        // Get the latest version for each major
+        const latestPerMajor = {}
+        Object.keys(versionsByMajor).forEach(major => {
+          const majorVersions = versionsByMajor[major]
+
+          // For the current latest major, use the dist-tags.latest version
+          if (parseInt(major) === latestMajor) {
+            latestPerMajor[major] = latestVersion
+          } else {
+            // For other majors, use the highest semver version
+            majorVersions.sort(semver.rcompare)
+            latestPerMajor[major] = majorVersions[0]
+          }
+        })
+
+        output[packageName] = latestPerMajor
       } else {
         output[packageName] = null
       }
--- services/main/test/routes/packages.test.js
+++ services/main/test/routes/packages.test.js
@@ -34,6 +34,9 @@ test('get package versions', async (t) => {
         data: {
           'dist-tags': {
             latest: versionToReturn
+          },
+          versions: {
+            [versionToReturn]: {}
           }
         }
       }
@@ -55,10 +58,129 @@ test('get package versions', async (t) => {
   assert.strictEqual(res.statusCode, 200)
   assert.deepEqual(ret, {
     package_versions: {
-      '@platformatic/runtime': '1.1.1',
-      '@platformatic/db': '2.2.2',
-      '@platformatic/composer': '3.3.3',
-      '@platformatic/service': '4.4.4'
+      '@platformatic/runtime': { 1: '1.1.1' },
+      '@platformatic/db': { 2: '2.2.2' },
+      '@platformatic/composer': { 3: '3.3.3' },
+      '@platformatic/service': { 4: '4.4.4' }
+    }
+  })
+})
+
+test('get package versions returns latest per major version', async (t) => {
+  // Create a fresh MockAgent for this test
+  const testAgent = new MockAgent()
+  setGlobalDispatcher(testAgent)
+
+  const client = testAgent.get('https://registry.npmjs.org')
+  client.intercept({
+    method: 'GET',
+    path: /.*/
+  })
+    .reply((req) => {
+      let versionsData
+      switch (req.path) {
+        case '/@platformatic/runtime':
+          versionsData = {
+            versions: {
+              '1.0.0': {},
+              '1.0.1': {},
+              '1.52.0': {},
+              '1.52.1': {},
+              '1.52.2': {},
+              '1.53.0': {},
+              '2.0.0': {},
+              '2.1.0': {},
+              '2.75.0': {},
+              '2.75.1': {},
+              '2.75.2': {}
+            },
+            'dist-tags': {
+              latest: '2.75.2'
+            }
+          }
+          break
+        case '/@platformatic/db':
+          versionsData = {
+            versions: {
+              '1.0.0': {},
+              '1.52.2': {},
+              '2.0.0': {},
+              '2.75.2': {}
+            },
+            'dist-tags': {
+              latest: '2.75.2'
+            }
+          }
+          break
+        case '/@platformatic/composer':
+          versionsData = {
+            versions: {
+              '1.0.0': {},
+              '1.52.2': {},
+              '2.0.0': {},
+              '2.75.2': {}
+            },
+            'dist-tags': {
+              latest: '2.75.2'
+            }
+          }
+          break
+        case '/@platformatic/service':
+          versionsData = {
+            versions: {
+              '1.0.0': {},
+              '1.52.2': {},
+              '2.0.0': {},
+              '2.75.2': {}
+            },
+            'dist-tags': {
+              latest: '2.75.2'
+            }
+          }
+          break
+      }
+      return {
+        statusCode: 200,
+        data: versionsData
+      }
+    }).persist()
+
+  const app = await getServer(t)
+  await app.start()
+
+  // Mock authorization
+  const { authorizeEndpoint } = require('../helper')
+  authorizeEndpoint(testAgent, 'GET', '/api/package_versions')
+
+  // Test - should return latest version for each major
+  const res = await app.inject({
+    method: 'GET',
+    headers: {
+      cookie: 'auth-cookie-name=fake-cookie'
+    },
+    url: '/api/package_versions'
+  })
+
+  const ret = res.json()
+  assert.strictEqual(res.statusCode, 200)
+  assert.deepEqual(ret, {
+    package_versions: {
+      '@platformatic/runtime': {
+        1: '1.53.0',
+        2: '2.75.2'
+      },
+      '@platformatic/db': {
+        1: '1.52.2',
+        2: '2.75.2'
+      },
+      '@platformatic/composer': {
+        1: '1.52.2',
+        2: '2.75.2'
+      },
+      '@platformatic/service': {
+        1: '1.52.2',
+        2: '2.75.2'
+      }
     }
   })
 })
